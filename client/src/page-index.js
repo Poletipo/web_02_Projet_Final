9fa69a9f1bbeb4f717839d13b55167dc196433ee
Jim <tijimjim@hotmail.com>
2020-08-26 11:28:21 -0400

    Title screen

--- client/src/page-index.js
+++ client/src/page-index.js
@@ -1,7 +1,35 @@
 import {signin} from './chat-api';
 
+let titleScreen = new Image();
+let canvas = null;
+let ctx = null;
+
+
 window.addEventListener("load", () => {
     document.querySelector("form").onsubmit = function () {
         return signin(this);
     }
+
+    canvas = document.querySelector("canvas");
+    ctx = canvas.getContext("2d");
+    titleScreen.src = "./img/NES - The Legend of Zelda - Title Screen.png";
+    //titleScreen.width = 1920 + "px";
+    ctx.webkitImageSmoothingEnabled = false;
+    ctx.mozImageSmoothingEnabled = false;
+    ctx.imageSmoothingEnabled = false;
+    
+    console.log("doot");
+    
+    
+    window.requestAnimationFrame(tick);
 });
+
+
+const tick = evt =>{
+    if(titleScreen.complete){
+        ctx.drawImage(titleScreen,0,0, canvas.width, canvas.height);
+    }
+    
+    
+    window.requestAnimationFrame(tick);
+}
